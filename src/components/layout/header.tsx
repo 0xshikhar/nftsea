3338565b7e5d3c70d86347799a14de11fcb1cd4c
Shikhar Singh <0xshikhar@gmail.com>
2025-04-01 18:01:43 +0530

    feat: add logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
                 <div className="flex items-center gap-2 md:gap-10">
                     <Link href="/" className="hidden items-center space-x-2 md:flex">
                         <Image
-                            src="/logo.svg"
+                            src="/logo.png"
                             alt="NFTsea logo"
                             width={32}
                             height={32}
